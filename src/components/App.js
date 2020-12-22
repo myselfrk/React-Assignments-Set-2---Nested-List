c6447c9431f02d54598b61e4bce94cdaf13f9bb6
Rohit Kumar <myselfrk0@gmail.com>
2020-12-22 16:46:52 +0530

    c2

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component, useState } from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import "./../styles/App.css";
+import State from "./state";
 
 // Do not alter the states const and values inside it.
 const states = [
@@ -154,84 +155,6 @@ const states = [
     ],
   },
 ];
-
-function Towns({ towns }) {
-  return (
-    <h3>
-      {towns.map((town, index) => (
-        <span
-          key={town.name}
-          id={`town${index + 1}`}
-          className="badge badge-primary m-2">
-          {town.name}
-        </span>
-      ))}
-    </h3>
-  );
-}
-
-function Cities({ cities }) {
-  const [selected, setSelected] = useState(-1);
-
-  const handleClick = ({ target }) => {
-    let newSelected = -1;
-    cities.forEach(function (item, index) {
-      if (item.name === target.innerText) newSelected = index;
-    });
-
-    if (selected === newSelected) {
-      setSelected(-1);
-    } else {
-      setSelected(newSelected);
-    }
-  };
-
-  return (
-    <div>
-      {cities.map((city, index) => (
-        <button
-          key={city.name}
-          id={`city${index + 1}`}
-          onClick={handleClick}
-          className="btn btn-primary m-2">
-          {city.name}
-        </button>
-      ))}
-      {selected !== -1 && <Towns towns={cities[selected].towns} />}
-    </div>
-  );
-}
-
-function State({ states }) {
-  const [selected, setSelected] = useState(-1);
-
-  const handleClick = ({ target }) => {
-    let newSelected = -1;
-    states.forEach(function (item, index) {
-      if (item.name === target.innerText) newSelected = index;
-    });
-
-    if (selected === newSelected) {
-      setSelected(-1);
-    } else {
-      setSelected(newSelected);
-    }
-  };
-  return (
-    <div>
-      {states.map((state, index) => (
-        <button
-          key={state.name}
-          onClick={handleClick}
-          id={`state${index + 1}`}
-          className="btn btn-primary m-2">
-          {state.name}
-        </button>
-      ))}
-      {selected !== -1 && <Cities cities={states[selected].cities} />}
-    </div>
-  );
-}
 
 function App() {
   return (
